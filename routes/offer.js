bc078cd6d2c7247abab2d475a36dc20643f25f09
slledru <slledru@msn.com>
2018-04-02 14:24:11 -0600

    added offer route

---
+++ routes/offer.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+/* GET all offers. */
+router.get('/', (req, res, next) => {
+  res.send('Get all offers')
+})
+
+// GET a single offer
+router.get('/:id', (req, res, next) => {
+  res.send('GET a single off')
+})
+
+// POST a new offer
+router.post('/', (req, res, next) => {
+  res.send('POST a new offer')
+})
+
+// DELETE an offer
+router.delete('/:id', (req, res, next) => {
+  res.send('DELETE an offer')
+})
+
+// UPDATE an offer
+router.patch('/:id', (req, res, next) => {
+  res.send('PATCH an offer')
+})
+
+module.exports = router
